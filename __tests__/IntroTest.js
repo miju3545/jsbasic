e6df8f5746d21db9f363b268b5d47bb2cc1c02b3
rovxxmjxx <miju3545@gmail.com>
2022-11-09 21:46:52 +0900

    feat: added Histogram test code

--- __tests__/IntroTest.js
+++ __tests__/IntroTest.js
@@ -1,9 +1,30 @@
-const DefaultMap = "../00-intro/utils/defaultMap";
+const { DefaultMap, Histogram } = require("../00-intro/utils");
 
 describe("DefaultMap 테스트", () => {
   test("get default value", () => {
-    const map = new DefaultMap("default...");
+    const map = new DefaultMap(0);
     const result = map.get();
-    expect(result).toEqual("default...");
+    expect(result).toEqual(0);
+  });
+
+  test("get exising value", () => {
+    const map = new DefaultMap(0);
+    const key = "ball";
+    const value = map.get(key);
+    map.set(key, value);
+    expect(map.get(key)).toEqual(value);
+  });
+});
+
+describe("Histogram 테스트", () => {
+  test("문자열을 맵 객체에 넣기", () => {
+    const hist = new Histogram();
+    const string = "rovxxmjxx";
+    hist.add(string);
+    const counts = hist.letterCounts;
+    const map = [1, 1, 1, 4, 1, 1];
+    for (let [key, value] of [...counts]) {
+      expect(counts.get(key)).toEqual(value);
+    }
   });
 });
